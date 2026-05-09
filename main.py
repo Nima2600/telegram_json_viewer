a573c0ea10a454bf186a4efdcc8246438bacb5ea
Nima Nazari <154949193+Nima2600@users.noreply.github.com>
2026-05-09 21:42:40 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,32 +5,40 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-
+# For wipe html tags and username
+# Need Upgrade!
 def wipetxt(content):
     wiped = BeautifulSoup(content, "html.parser").get_text()
     cleaned = re.sub(r"@[\w.-]+", "", wiped)
     return cleaned
 
+# Convert UTC to TEH +3:30
 def utc2teh(time):
     utc_time = datetime.fromisoformat(time)
     tehran = utc_time.astimezone(tz('Asia/Tehran'))
     print(f"{tehran}\n\n")
 
+# Catch channel name
 def name_catch(url):
     url_name = url.split("/")[-1].split(".")[0]
     return url_name
-    
+
+# URLs for script
 with open('urls.txt', 'r') as file:
     urls = file.read().splitlines()	
 
+
 for url in urls:
+    # For skip specific URL, put # at the first of URL
+    if url[0] == "#":
+        continue
     try:
         usr = input(f"{name_catch(url)} >> ")
         if usr == "n":
             continue
         response = requests.get(url)
-        response.raise_for_status()  # بررسی خطا در دریافت
-        data = json.loads(response.text)  # تبدیل محتوا به دیکشنری Python
+        response.raise_for_status()
+        data = json.loads(response.text)
         print("="*80)
         for post in data['posts'][::-1]:
             ussr = input("\n\n==================\n--- Press Enter ---\n==================\n >> ")
